50b5574d6d5b7de28107a09c6232db886316d495
Michael <mtyylx@gmail.com>
2017-03-19 22:31:54 +0800

    V1.0.0: Now support up to FOUR National Parks. Provide command line UI, user can search park lodge info on a given date and browser option (head/headless).

---
+++ run.py
@@ -0,0 +1,24 @@
+from LodgeScanner import LodgeScanner
+
+scanner = LodgeScanner()
+print """
+Please enter search info, separated by comma:
+Format: PARK,YEAR,MONTH,DAY,METHOD
+PARK:
+    Grand Canyon = gc
+    Crater Lake = cl
+    Zion Park = zp
+    Glacier Park = gp
+METHOD:
+    PhantomJS = p   (Headless Browser)
+    Chrome = c      (Normal Browser)
+
+Example:
+    gc,2017,4,9,p
+    zp,2017,4,3,c
+"""
+r = raw_input(">>> Enter Here: ")
+if r is not "":
+    r = r.split(",")
+    scanner.config_scanner(r[0], r[1], r[2], r[3], r[4])
+scanner.run()
